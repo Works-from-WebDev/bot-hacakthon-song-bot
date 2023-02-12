76be591da240c21756d95ba76f639092c3771143
johnny shafir <97397410+Josnon@users.noreply.github.com>
2023-02-12 17:06:53 +0200

    bot

--- bot.py
+++ bot.py
@@ -1,20 +1,70 @@
-import logging
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+import requests as req
+from keys import KEY
+bot = telebot.TeleBot(KEY)
+stringList = {'start': ['Play', 'Settings', 'Help'],
+              'settings': ['Language', 'Theme', 'Back'],
+              'language': ['English', 'Hebrew', 'Back'],
+              'theme': ['Dark', 'Light', 'Back'],
+              'help': ['How to play', 'About', 'Back']
+              }
 
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton(stringList['start'][0], callback_data='play'),
+               InlineKeyboardButton(
+                   stringList['start'][1], callback_data='settings'),
+               InlineKeyboardButton(
+                   stringList['start'][2], callback_data='help'),
+               )
+    bot.send_message(message.chat.id, 'Welcome to the game!',
+                     reply_markup=markup)
 
 
+@bot.callback_query_handler(func=lambda call: True)
+def process_callback(call):
+    if call.data == 'play':
 
-logging.basicConfig(
-    format="[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s",
-    level=logging.INFO,
-)
+        bot.send_message(call.message.chat.id, 'Play')
 
-logger = logging.getLogger(__name__)
+    elif call.data == 'settings':
 
-)
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(stringList['settings'][0], callback_data='language'),
+                   InlineKeyboardButton(
+                       stringList['settings'][1], callback_data='theme'),
+                   InlineKeyboardButton(
+                       stringList['settings'][2], callback_data='back'))
+        bot.send_message(call.message.chat.id, 'Settings', reply_markup=markup)
+    elif call.data == 'language':
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(stringList['language'][0], callback_data='english'),
+                   InlineKeyboardButton(
+                       stringList['language'][1], callback_data='hebrew'),
+                   InlineKeyboardButton(stringList['language'][2], callback_data='back'))
+        bot.send_message(call.message.chat.id, 'Language', reply_markup=markup)
+
+    elif call.data == 'help':
+
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(stringList['help'][0], callback_data='howtoplay'),
+                   InlineKeyboardButton(
+                       stringList['help'][1], callback_data='about'),
+                   InlineKeyboardButton(stringList['help'][2], callback_data='back'))
+        bot.send_message(call.message.chat.id, 'Help', reply_markup=markup)
+    elif call.data == 'back':
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(stringList['start'][0], callback_data='play'),
+                   InlineKeyboardButton(
+                       stringList['start'][1], callback_data='settings'),
+                   InlineKeyboardButton(
+                       stringList['start'][2], callback_data='help'),
+                   )
+        bot.send_message(call.message.chat.id,
+                         'Welcome to the game!', reply_markup=markup)
 
 
-
-
-logger.info("* Start polling...")
-logger.info("* Bye!")
+bot.polling()
