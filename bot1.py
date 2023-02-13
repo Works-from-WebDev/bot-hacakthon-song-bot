96215f528aaf7c637b325df4e83dd83c3ce95bcf
idan <idan.tepe15@gmail.com>
2023-02-13 18:03:51 +0200

    wip success and fail messages

--- bot1.py
+++ bot1.py
@@ -13,6 +13,10 @@ from telegram.ext import (
 
 import bot_settings
 
+# settings
+interval = 5
+times = 30
+
 logging.basicConfig(
     format="[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s",
     level=logging.INFO,
@@ -21,46 +25,46 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+class State:
+    def __init__(self, chat_id, name, artist, lyrics):
+        self.chat_id = chat_id
+        self.name = name
+        self.artist = artist
+        self.lyrics = lyrics
+        self.running = True
+        self.counter = 0
+
+    def __repr__(self):
+        return f"Song(name={self.name!r}, chat_id={self.chat_id})"
+
+
 def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
-    if context.user_data.get("song"):
+    if context.user_data.get("song") and context.user_data["song"].running:
         return
-    song = random_song("BOTH")
+
+    name, artist, lyrics = random_song("BOTH")
+    song = State(chat_id, name, artist, lyrics)
     context.user_data["song"] = song
-    context.user_data["running"] = [True]
-    logger.info(f"> Start chat #{chat_id},song ={song[0]}")
+    logger.info(f"> Start chat #{chat_id},song={song}")
     context.bot.send_message(
         chat_id=chat_id, text=f"Hi welcome to the Guessing The Song game!!!"
     )
 
-    j.run_once(
-        callback_minute,
-        2,
-        context={
-            "chat_id": update.message.chat_id,
-            "counter": 0,
-            "song": song,
-            "running": context.user_data["running"],
-        },
-    )
+    j.run_once(callback_minute, 1.5, context=song)
 
 
 def callback_minute(context: telegram.ext.CallbackContext):
-    d = context.job.context
-    if not d["running"][0]:
+    song: State = context.job.context
+    if not song.running:
         return
-    song = d["song"]
-    context.bot.send_message(chat_id=d["chat_id"], text=song[2][d["counter"]])
-    j.run_once(
-        callback_minute,
-        2,
-        context={
-            "chat_id": d["chat_id"],
-            "counter": d["counter"] + 1,
-            "song": song,
-            "running": d["running"],
-        },
-    )
+    if song.counter > times:
+        context.bot.send_message(chat_id=song.chat_id, text="sorry you lost")
+        song.running = False
+        return
+    context.bot.send_message(chat_id=song.chat_id, text=song.lyrics[song.counter])
+    song.counter += 1
+    j.run_once(callback_minute, interval, context=song)
 
 
 def guess(update: Update, context: CallbackContext):
@@ -69,12 +73,19 @@ def guess(update: Update, context: CallbackContext):
     song = context.user_data.get("song")
     if not song:
         return
-    success = text.lower() == song[0].lower()
-    msg = "You got it!!!" if success else f"{text} is not the song's name"
+    if not song.running:
+        return
+
+    success = text.lower() == song.name.lower()
+    msg = (
+        f"You got it!!! the name of the song is: {song.name} by the artist: {song.artist}"
+        if success
+        else f"{text} is not the song's name"
+    )
     context.bot.send_message(chat_id=update.message.chat_id, text=msg)
     logger.info(f"= Got on chat #{chat_id}: {text!r} {success=}")
     if success:
-        context.user_data["running"][0] = False
+        song.running = False
 
 
 my_bot = Updater(token=bot_settings.BOT_TOKEN, use_context=True)
