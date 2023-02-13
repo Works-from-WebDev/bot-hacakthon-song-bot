96ad379eff186241cc48292385ec72179a6b0f3d
Ran-E <rany01@gmail.com>
2023-02-13 11:54:22 +0200

    Update BD

--- db.py
+++ db.py
@@ -5,20 +5,12 @@ client = MongoClient()
 db = client.get_database("sing_bot")
 users = db.get_collection("users")
 
-# Delete all documents in the collection
-users.delete_many({})
-
 
 # Function to handle updates or inserts into the MongoDB database
 def handle_update_or_insert(chat_id, user_name, score):
     document = {"chat_id": chat_id, "user_name": user_name, "score": score}
     users.update_one({"chat_id": chat_id}, {"$set": document}, upsert=True)
 
-
-# Example usage of the function
-handle_update_or_insert("chat1", "User 1", 100)
-handle_update_or_insert("chat2", "User 2", 95)
-handle_update_or_insert("chat1", "Updated User 1", 120)
 
 # Get all documents in the collection
 documents = users.find()
