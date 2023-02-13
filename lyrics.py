8a538791b2a744d25fe24ab11f46e914f860fcd3
idan-tepe <117281221+idan-tepe@users.noreply.github.com>
2023-02-13 11:20:37 +0200

    Add files via upload

---
+++ lyrics.py
@@ -0,0 +1,31 @@
+import lyricsgenius
+import bot_settings
+from all_songs import singers
+import random
+import re
+
+
+def random_song():
+    keys = list(singers)
+    artist = random.choice(keys)
+    song = random.choice(singers[artist])
+    ly = lyrics(artist, song)
+    ly = re.sub("\n", " ", ly, )
+    ly = re.sub(r"(.*)(Lyrics)", "", ly)
+    ly = re.sub(r'[^\w\s]', '', ly)
+    ly = ly.split()
+    return [song, artist, ly[:-1]]
+
+
+def lyrics(artist, song):
+    genius = lyricsgenius.Genius(bot_settings.API_KEY)
+    genius.verbose = False
+    genius.remove_section_headers = True
+    song = genius.search_song(song, artist)
+    return song.lyrics
+
+
+
+# print(lyrics('As Long as You Love Me', 'Justin Bieber'))
+
+print(random_song())
